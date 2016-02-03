d6025d1d16413c7c669b6fc46bf37900888f63ac
brianvital <brianvital@hotmail.com>
2016-02-03 09:50:05 -0600

    thisinitial

---
+++ MoviesWebAPIDup/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MoviesWebAPIDup.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MoviesWebAPIDup/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MoviesWebAPIDup.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
